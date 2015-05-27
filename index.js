6e0dce136fe4922dfaf99933bb36bdc72ab96ae9
Maciej Pawlik <m.pawlik@cyfronet.pl>
2015-05-27 17:57:17 +0200

    Remove builder skel.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 var client = require('./lib/client.js');
-var builder = require('./lib/builder.js');
 
 module.exports.MetricClient = client.MetricClient;
-module.exports.MetricBuilder = builder.MetricBuilder;
